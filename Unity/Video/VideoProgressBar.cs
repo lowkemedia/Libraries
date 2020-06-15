89cc227b82bf6f23a61fc60381369619cf40af45
Russell Lowke <russ@lowkemedia.com>
2020-06-15 15:16:17 +1000

    Create VideoProgressBar.cs

---
+++ Unity/Video/VideoProgressBar.cs
@@ -0,0 +1,76 @@
+﻿//
+//  VideoController - Video package
+//  Russell Lowke, June 15th 2020
+//
+//  Copyright (c) 2020 Lowke Media
+//  see http://www.lowkemedia.com for more information
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//  DEALINGS IN THE SOFTWARE.
+//
+//
+
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoProgressBar : MonoBehaviour, IDragHandler, IPointerDownHandler
+{
+    public VideoPlayer videoPlayer;
+
+    private Image _progress;
+    
+    private void Awake()
+    {
+        _progress = GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+		if (videoPlayer.frameCount > 0) {
+			_progress.fillAmount = (float)videoPlayer.frame / videoPlayer.frameCount;
+		}
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        TrySkip(eventData);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        TrySkip(eventData);
+    }
+
+    private void TrySkip(PointerEventData eventData)
+    {
+		if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+			_progress.rectTransform, eventData.position, null, out Vector2 localPoint)) {
+			float pct = Mathf.InverseLerp(_progress.rectTransform.rect.xMin, _progress.rectTransform.rect.xMax, localPoint.x);
+			SkipToPercent(pct);
+		}
+	}
+
+    private void SkipToPercent(float pct)
+    {
+        var frame = videoPlayer.frameCount * pct;
+        videoPlayer.frame = (long)frame;
+    }
+}
