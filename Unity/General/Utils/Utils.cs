a343b0d2c41d7243d300c6215848470fa257b858
Russell Lowke <russ@lowkemedia.com>
2020-10-17 14:57:49 +1100

    Adding IsDestroyed() method to Utils

--- Unity/General/Utils/Utils.cs
+++ Unity/General/Utils/Utils.cs
@@ -112,4 +112,13 @@ public static class Utils
         }
         return output;
     }
+
+    public static bool IsDestroyed(GameObject gameObject)
+    {
+        // UnityEngine overloads the == opeator for the GameObject type
+        // and returns null when the object has been destroyed, but 
+        // actually the object is still there but has not been cleaned up yet
+        // if we test both we can determine if the object has been destroyed.
+        return gameObject == null && !ReferenceEquals(gameObject, null);
+    }
 }
