2279466d96d5a2b90cfe48dc66f18cc2a8300beb
Russell Lowke <russ@lowkemedia.com>
2021-06-13 17:37:36 +0930

    Update SetOfFonts.cs

--- Unity/General/Localizer/Stylizer/SetOfFonts.cs
+++ Unity/General/Localizer/Stylizer/SetOfFonts.cs
@@ -7,27 +7,28 @@ using LocalizerTypes;
 public class SetOfFonts : ScriptableObject
 {
     // Primary fonts
-    public TMP_FontAsset goudy_lombardic_capitals_116;
-    public TMP_FontAsset friz_quadrata_italic_32;
-    public TMP_FontAsset friz_quadrata_italic_42;
-    public TMP_FontAsset friz_quadrata_regular_36;
-    public TMP_FontAsset friz_quadrata_regular_42;
-    public TMP_FontAsset friz_quadrata_regular_52;
+    public TMP_FontAsset black_castle_46;
+    public TMP_FontAsset black_castle_96;
+    public TMP_FontAsset black_castle_150;
+
+    public TMP_FontAsset bookerly_italic_32;
+    public TMP_FontAsset bookerly_regular_38;
+    public TMP_FontAsset bookerly_regular_38_b_i;
+    public TMP_FontAsset bookerly_regular_50;
+
 
     // Russian fonts - Cyrillic
-    public TMP_FontAsset oswald_110;
-    public TMP_FontAsset ubuntu_32;
-    public TMP_FontAsset ubuntu_36;
-    public TMP_FontAsset ubuntu_42;
-    public TMP_FontAsset ubuntu_italic_32;
-    public TMP_FontAsset ubuntu_italic_36;
-    public TMP_FontAsset ubuntu_italic_42;
+    public TMP_FontAsset oswald_46;
+    public TMP_FontAsset oswald_96;
+    public TMP_FontAsset oswald_150;
+
 
     // Japanese fonts - Kanji
     public TMP_FontAsset hiragino_mincho_32;
-    public TMP_FontAsset hiragino_mincho_36;
-    public TMP_FontAsset hiragino_mincho_42;
-    public TMP_FontAsset hiragino_mincho_112;
+    public TMP_FontAsset hiragino_mincho_38;
+    public TMP_FontAsset hiragino_mincho_50;
+    public TMP_FontAsset hiragino_mincho_96;
+
 
     public static SetOfFonts _instance;
     public static SetOfFonts Instance {
@@ -46,45 +47,49 @@ public class SetOfFonts : ScriptableObject
     {
         switch (font) {
 
+            //
             // Primary fonts
-            case "Goudy Text MT Lombardic Capitals SDF 116pt":
-                return goudy_lombardic_capitals_116;
-            case "Friz Quadrata Regular Italic SDF 32pt":
-                return friz_quadrata_italic_32;
-            case "Friz Quadrata Regular Italic SDF 42pt":
-                return friz_quadrata_italic_42;
-            case "Friz Quadrata Regular SDF 36pt":
-                return friz_quadrata_regular_36;
-            case "Friz Quadrata Regular SDF 42pt":
-                return friz_quadrata_regular_42;
-            case "Friz Quadrata Regular SDF 52pt":
-                return friz_quadrata_regular_52;
 
+            //  title font
+            case "BlackCastleMF SDF 46 px":
+                return black_castle_46;
+            case "BlackCastleMF SDF 96 px":
+                return black_castle_96;
+            case "BlackCastleMF SDF 150 px":
+                return black_castle_150;
+
+            //  body font
+            case "Bookerly-RegularItalic SDF 32 px":
+                return bookerly_italic_32;
+            case "Bookerly-Regular SDF 38 px":
+                return bookerly_regular_38;
+            case "Bookerly-Regular SDF 38 px bolded-italics":
+                return bookerly_regular_38_b_i;
+            case "Bookerly-Regular SDF 50 px":
+                return bookerly_regular_50;
+
+            //
             // Russian fonts - Cyrillic
-            case "Oswald-VariableFont_wght SDF 110pt":
-                return oswald_110;
-            case "Ubuntu-Regular SDF 32pt":
-                return ubuntu_32;
-            case "Ubuntu-Regular SDF 36pt":
-                return ubuntu_36;
-            case "Ubuntu-Regular SDF 42pt":
-                return ubuntu_42;
-            case "Ubuntu-Italic SDF 32pt":
-                return ubuntu_italic_32;
-            case "Ubuntu-Italic SDF 36pt":
-                return ubuntu_italic_36;
-            case "Ubuntu-Italic SDF 42pt":
-                return ubuntu_italic_42;
 
+            case "Oswald-VariableFont_wght SDF 46 px":
+                return oswald_46;
+            case "Oswald-VariableFont_wght SDF 96 px":
+                return oswald_96;
+            case "Oswald-VariableFont_wght SDF 150 px":
+                return oswald_150;
+
+            //
             // Japanese fonts - Kanji
-            case "Hiragino Mincho ProN SDF 32pt":
+
+            case "Hiragino Mincho ProN SDF 32 px":
                 return hiragino_mincho_32;
-            case "Hiragino Mincho ProN SDF 36pt":
-                return hiragino_mincho_36;
-            case "Hiragino Mincho ProN SDF 42pt":
-                return hiragino_mincho_42;
-            case "Hiragino Mincho ProN SDF 112pt":
-                return hiragino_mincho_112;
+            case "Hiragino Mincho ProN SDF 38 px":
+                return hiragino_mincho_38;
+            case "Hiragino Mincho ProN SDF 50 px":
+                return hiragino_mincho_50;
+            case "Hiragino Mincho ProN SDF 96 px":
+                return hiragino_mincho_96;
+
 
             default:
                 Logger.Severe("Could not find font " + font, LocalizerID.SEVERE_CANT_FIND_FONT, true);
