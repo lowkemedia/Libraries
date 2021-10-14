b351007d73c11bf2c8d7485e4c53a6d550318710
Russell Lowke <russ@lowkemedia.com>
2021-10-14 20:36:53 +1030

    Adding ResetArray() to UtilsArray

--- Unity/General/Utils/UtilsArray.cs
+++ Unity/General/Utils/UtilsArray.cs
@@ -30,8 +30,20 @@ using System.Linq;
 
 public static class UtilsArray
 {
+    public static void ResetArray<T>(this T[] array)
+    {
+        for (int i = 0; i < array.Length; ++i) {
+            // default(T) will return the default value for whatever type T is
+            // For example, if T is an int, default(T) would return 0
+            array[i] = default(T);
+        }
+    }
+
     public static T[] CloneArray<T>(T[] array)
     {
+        if (array == default) {
+            return default;
+        }
         return (T[])array.Clone();
     }
 
