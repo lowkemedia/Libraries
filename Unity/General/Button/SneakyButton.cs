3dee4944a8b67e2d4086c39f25f7b0b346bf5046
Russell Lowke <russ@lowkemedia.com>
2022-11-07 15:48:59 +1030

    Adding SneakyButton

---
+++ Unity/General/Button/SneakyButton.cs
@@ -0,0 +1,66 @@
+﻿//
+//  PressButton - Button package
+//  Russell Lowke, November 1st 2022
+//
+//  Copyright (c) 2022 Lowke Media
+//  see https://github.com/lowkemedia/Libraries for more information
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//  DEALINGS IN THE SOFTWARE.
+//
+//
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SneakyButton : ClickButton
+{
+    private float _pressTime;                           // time of last press
+    public float holdDuration = 1.0f;                   // duration button must be held
+
+    public override void OnPointerDown(PointerEventData pointerEventData = null)
+    {
+        _pressTime = Time.time;
+    }
+
+    public override void OnPointerClick(PointerEventData pointerEventData)
+    {
+    }
+
+    public override void OnPointerUp(PointerEventData pointerEventData)
+    {
+    }
+
+    public override void OnPointerEnter(PointerEventData pointerEventData)
+    {
+    }
+
+    public override void OnPointerExit(PointerEventData pointerEventData)
+    {
+    }
+
+    private void Update()
+    {
+        float timeNow = Time.time;
+        if (_pressTime != default &&
+            timeNow > _pressTime + holdDuration) {
+            _pressTime = default;
+            Click();
+        }
+    }
+}
